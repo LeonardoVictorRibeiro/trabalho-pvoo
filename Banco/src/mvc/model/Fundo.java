23d90c0c495e0637a91e87272b0b08d7eb7790ed
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2018-11-09 11:34:12 -0200

    Adicionado java beans

--- Banco/src/mvc/model/Fundo.java
+++ Banco/src/mvc/model/Fundo.java
@@ -5,10 +5,72 @@
  */
 package mvc.model;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author aluno
  */
 public class Fundo {
+    
+    private long id;
+    private String nome;
+    private BigDecimal saldototal;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSaldototal() {
+        return saldototal;
+    }
+
+    public void setSaldototal(BigDecimal saldototal) {
+        this.saldototal = saldototal;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fundo other = (Fundo) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Fundo{" + "id=" + id + ", nome=" + nome + ", saldototal=" + saldototal + '}';
+    }
+    
+    
     
 }
