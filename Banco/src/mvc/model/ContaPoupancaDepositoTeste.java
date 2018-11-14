73a11aa2d26275c203f64ba18dec12a6ca80882b
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2018-11-14 02:12:14 -0200

    Modifica classe que testa os depósitos na conta poupanca

--- Banco/src/mvc/model/ContaPoupancaDepositoTeste.java
+++ Banco/src/mvc/model/ContaPoupancaDepositoTeste.java
@@ -3,25 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mvc.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.List;
 
 /**
- * 
+ *
  * @author Leonardo Victor Ribeiro <leonardovictor@outlook.com>
  */
 public class ContaPoupancaDepositoTeste {
-    
+
     public static void main(String[] args) {
+        //DAOs necessários
         ClienteDAO cliDAO = new ClienteDAO();
         ContaPoupancaDAO cpDAO = new ContaPoupancaDAO();
-        
-        ContaPoupanca cp1 = cpDAO.encontrarConta(new ContaPoupanca(5), cliDAO);
-        
-        ContaPoupancaDeposito cpd1 = new ContaPoupancaDeposito(cp1, new BigDecimal("300"), LocalDate.now(), LocalDate.now().plusDays(30));
+        ContaPoupancaDepositoDAO cpDepositoDAO = new ContaPoupancaDepositoDAO();
+
+        //Listar depósitos de uma conta
+        ContaPoupanca cp = cpDAO.encontrarConta(new ContaPoupanca(1), cliDAO);
+        //Este método retorna uma lista com os depósitos
+        List<ContaPoupancaDeposito> cpDepositos = cpDepositoDAO.listarDepositosDeUmaConta(cp);
+        //Mostra os depósitos
+        for (ContaPoupancaDeposito cpDeposito : cpDepositos) {
+            System.out.println(cpDeposito);
+        }
+
+        //Listar todos os depósitos ( de todas as contas ) encontrados no banco 
+        List<ContaPoupancaDeposito> todosDepositos = cpDepositoDAO.listar(cpDAO, cliDAO);
+
+        for (ContaPoupancaDeposito umDeposito : todosDepositos) {
+            System.out.println(umDeposito);
+        }
     }
 
 }
